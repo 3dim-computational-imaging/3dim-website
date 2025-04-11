3d83aba52d6ffc7a20f55fde07447482cd49fe8a
John Bass <spydercrawler@gmail.com>
2025-04-11 15:56:35 -0700

    Added javascript to allow collapsible linking

--- static/scripts/collapsible-section.js
+++ static/scripts/collapsible-section.js
@@ -13,3 +13,27 @@ for (i = 0; i < collapsible_buttons.length; i++) {
     }
   });
 } 
+
+window.addEventListener('hashchange', function() {
+  const pageId = window.location.hash.substring(1); // Remove the '#'
+  if (pageId) {
+	openLinkedCollapsibleSection(pageId);
+  }
+});
+
+if (window.location.hash) {
+  const pageId = window.location.hash.substring(1);
+  openLinkedCollapsibleSection(pageId);
+}
+
+function openLinkedCollapsibleSection(pageId) {
+	pageElement = document.getElementById(pageId);
+	if(pageElement.classList.contains("collapsible-section-shell")) {
+		for (var i = 0; i < pageElement.childNodes.length; i++) {
+			if (pageElement.childNodes[i].classList.contains("collapsible-section-header")) {
+				pageElement.childNodes[i].classList.add("active");
+				break;
+			}        
+		}
+	}
+}
