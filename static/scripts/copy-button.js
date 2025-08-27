8791d042f4fefbbbb5ebb456aa5b86656c4a867e
John Bass <spydercrawler@gmail.com>
2025-08-27 15:42:20 -0700

    Added alternative method for enabling link copying

--- static/scripts/copy-button.js
+++ static/scripts/copy-button.js
@@ -1,5 +1,15 @@
 // Function that will copy a link to the Clipboard
-function copyLink(linkURL) {
+var copy_buttons = document.getElementsByClassName("project-section-permalink-button");
+var i;
+
+for (i = 0; i < copy_buttons.length; i++) {
+  copy_buttons[i].addEventListener("click", function() {
+	ancestorWithSubprojectName = document.querySelector("p").closest(".project-section-container");
+	ancestorId = ancestorWithSubprojectName.id;
+	projectName = ancestorId.slice(11);
+	  
+	linkURL = window.location.href + projectName;
+	  
 	if ('clipboard' in navigator) {
       // Note: Clipboard API requires HTTPS or localhost
       navigator.clipboard.writeText(linkURL);
@@ -9,4 +19,5 @@ function copyLink(linkURL) {
       console.debug('Could not copy content!');
     }
 	return;
+  });
 }
