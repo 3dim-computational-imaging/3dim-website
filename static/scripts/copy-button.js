966a8c2e1881dd2670cdd36ea70ceb0ade41d9d3
John Bass <spydercrawler@gmail.com>
2025-08-27 15:55:57 -0700

    Fixed error in copy buttons

--- static/scripts/copy-button.js
+++ static/scripts/copy-button.js
@@ -13,8 +13,6 @@ for (i = 0; i < copy_buttons.length; i++) {
 	if ('clipboard' in navigator) {
       // Note: Clipboard API requires HTTPS or localhost
       navigator.clipboard.writeText(linkURL);
-      copiedNotification();
-      return;
     } else {
       console.debug('Could not copy content!');
     }
